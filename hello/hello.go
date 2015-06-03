758da6cb89d44dccf4914142f4682d9b30db058d
rubicks <naroza@gmail.com>
2015-06-03 16:59:59 -0400

    adds runtime.Caller invocation

--- hello/hello.go
+++ hello/hello.go
@@ -4,8 +4,14 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func main() {
+	pc, file, line, ok := runtime.Caller(0)
+	fmt.Println("pc	 : ", pc)
+	fmt.Println("file : ", file)
+	fmt.Println("line : ", line)
+	fmt.Println("ok	 : ", ok)
 	fmt.Println("Hello, Go!")
 }
