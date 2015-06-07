6a23f9b83841cf124637f3c910b003b83ea14cda
rubicks <naroza@gmail.com>
2015-06-07 17:13:59 -0400

    moves func Decr to decr

--- dumbmath/fib.go
+++ dumbmath/fib.go
@@ -6,14 +6,15 @@ import (
 	"math/big"
 )
 
-func Decr(n *big.Int) *big.Int {
+func decr(n *big.Int) *big.Int {
 	return n.Sub(n, big.NewInt(1))
 }
 
+// Fib returns the nth Fibonacci number
 func Fib(n *big.Int) *big.Int {
 	ret := big.NewInt(0)
 	nex := big.NewInt(1)
-	for ; 0 < n.Sign(); n = Decr(n) {
+	for ; 0 < n.Sign(); n = decr(n) {
 		ret, nex = nex, new(big.Int).Add(ret, nex)
 	}
 	return ret
