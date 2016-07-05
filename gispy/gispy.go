f65a995d9eebf5e0b1caf06ffe838c61939d8336
rubicks <naroza@gmail.com>
2016-07-05 19:33:34 +0000

    add gispy/gispy.go

---
+++ gispy/gispy.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/chzyer/readline"
+)
+
+func main() {
+	fmt.Println("Gispy v0.0.0")
+	fmt.Println("Press Ctrl+c to exit")
+	rl, err := readline.New("gispy> ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rl.Close()
+	for {
+		text, err := rl.Readline()
+		if nil != err {
+			break
+		}
+		println(text)
+	}
+}
